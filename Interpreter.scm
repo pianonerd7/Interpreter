96b37d70388ced77f638e458dee1e1556a3ad42b
pianonerd7 <pianonerd7@gmail.com>
2016-02-06 19:33:59 -0500

    first test passes...

--- Interpreter.scm
+++ Interpreter.scm
@@ -15,7 +15,26 @@
       ((eq? '% (front expression)) (remainder (M_value (middle expression)) (M_value (last expression))))
       (else (error 'unknown "unknown expression")))))
 
+(define M_return
+  (lambda (expression)
+    (cond
+      ((null? expression) '())
+      ((list? expression) (M_return (car expression)))
+      ((number? expression) expression)
+      (else '()))))
+
+(define M_state
+  (lambda (expression)
+    (cond
+      ((eq? 'return (car (car expression))) (M_return (cdr (car expression)))))))
+
+(define evaluate
+  (lambda (expressions state)
+    (if (null? expressions)
+        state
+        (M_state expressions))))
+
 (define interpret
   (lambda (filename)
-    (parser filename)))
-    
+    (evaluate (parser filename) '(()()))))
+
