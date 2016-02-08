4036584beb1b18042d361a6f316459d5fdaa10e2
pianonerd7 <pianonerd7@gmail.com>
2016-02-08 01:49:43 -0500

    abstraction

--- Interpreter.scm
+++ Interpreter.scm
@@ -22,57 +22,68 @@
       ((list? expression) (M_return (M_value (car expression))))
       (else (number? expression) expression))))
 
+
+(define variable car)
+(define value cdr)
 ;(M_declare '(x 12) '(()())) --> ((x) (12))
 ;(M_declare '(x) '(()())) --> ((x) (#f))
 (define M_declare
   (lambda (expression state)
     (cond
-      ((and (null? (cdr expression)) (eq? (searchVariable (car expression) state) #f)) (addVariable (car expression) #f state))
-      ((eq? (searchVariable (car expression) state) #f) (addVariable (car expression) (car (cdr expression)) state))
+      ((and (null? (value expression)) (eq? (searchVariable (variable expression) state) #f)) (addVariable (variable expression) #f state))
+      ((eq? (searchVariable (variable expression) state) #f) (addVariable (variable expression) (car (value expression)) state))
       (else (error 'unknown "unknown expression")))))
 
 (define M_assignment
   (lambda (expression state)
     (cond
-      ((null? expression) state))))
+      ((eq? (searchVariable (car expression)) #f)))))
 
+(define condition caar)
+(define body cdar)
 
 (define M_state
   (lambda (expression state)
     (cond
       ((null? expression) state)
-      ((eq? 'var (car (car expression))) (M_declare (cdr (car expression)) state))
-      ((eq? '= (car (car expression))) (M_assignment (cdr expression) state))
-      ((eq? 'return (car (car expression))) (M_return (cdr (car expression))))
+      ((eq? 'var (condition expression)) (M_declare (body expression) state))
+      ((eq? '= (condition expression)) (M_assignment (cdr expression) state))
+      ((eq? 'return (condition expression)) (M_return (body expression)))
       (else (error 'unknown "unknown expression")))))
 
+(define variables car)
+(define vals cadr)
+(define restOfVariables cdar)
+(define restOfValues cdadr)
+(define 1stVariable caar)
+(define 1stValue caadr)
 ;(searchVariable 'y '((x y z) (1 2 3))) --> 2
 (define searchVariable
   (lambda (var state)
     (cond
       ((null? (car state)) #f)
-      ((eq? var (car (car state))) (car (car (cdr state))))
+      ((eq? var (1stVariable state)) (1stValue state))
       (else (searchVariable var (removeFirstPair state))))))
 
 ;(addVariable 'x 4 '((z)(1))) --> ((z x) (1 4))
 (define addVariable
   (lambda (var value state)
-    (cons (append (car state) (cons var '()))(cons (append (car (cdr state)) (cons value '())) '()))))
+    (cons (append (variables state) (cons var '()))(cons (append (vals state) (cons value '())) '()))))
 
 ;(removeVariable 'y '((x y z) (1 2 3))) --> ((x z) (1 3))
 (define removeVariable
   (lambda (var state)
     (cond
-      ((null? (car state)) #f)
-      ((eq? var (car (car state))) (cons (cdr (car state)) (cons (cdr (car (cdr state))) '())))
-      (else (cons (cons (car (car state)) (car (removeVariable var (removeFirstPair state)))) (cons (cons (car (car (cdr state))) (car (cdr (removeVariable var (removeFirstPair state))))) '()))))))
+      ((null? (variables state)) #f)
+      ((eq? var (1stVariable state)) (cons (restOfVariables state) (cons (cdadr state) '())))
+      (else (cons (cons (1stVariable state) (variables (removeVariable var (removeFirstPair state)))) (cons (cons (1stValue state) (vals (removeVariable var (removeFirstPair state)))) '()))))))
 
 ;(removeFirstPair '((x y z)(4 5 6))) --> ((y z) (5 6))
 (define removeFirstPair
   (lambda (state)
     (if (null? (car state))
         state
-        (cons (cdr (car state))(cons (cdr (car (cdr state))) '())))))
+        (cons (restOfVariables state)(cons (restOfValues state) '())))))
 
 (define evaluate
   (lambda (expressions state)
