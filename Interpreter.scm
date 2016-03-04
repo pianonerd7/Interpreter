b88a52dc4820202e3bc15b1de5f1929419e175f7
pianonerd7 <pianonerd7@gmail.com>
2016-03-04 17:41:30 -0500

    nvm reverting

--- Interpreter.scm
+++ Interpreter.scm
@@ -19,8 +19,8 @@
       ((eq? 'begin (condition expression)) (M_state_Begin (body expression) state rtn break continue))
       ((eq? 'continue (condition expression)) (M_state_Continue continue state))
       ((eq? 'break (condition expression)) (M_state_Break break state))
-      ((eq? 'try (condition expression)) (M_state_Try (body expression) state))
-      ((eq? 'throw (condition expression)) (M_state(break state))
+      ((eq? 'try (condition expression)) (M_state_Try expression state))
+      ((eq? 'throw (condition expression)) (M_state(break state)))
       (else (M_boolean(expression) state)))))
 
 (define boolean_operator car)
@@ -152,59 +152,10 @@
   (lambda (break state)
     (break (removeTopLayer state))))
 
-(define tryBlock car)
-(define 2ndExpression cadr)
-(define 3rdExpression cddr)
-(define catchBody
-  (lambda (expression)
-    (caddr (2ndExpression expression))))
 (define M_state_Try
   (lambda (expression state)
-    (cond
-      ;case of try/catch and finally
-      ((and (eq? (car (2ndExpression expression)) 'catch) (pair? (3rdExpression expression))) (M_state_TryCatchFinally expression (addLayer initialState (consEmptyListToState state))))
-      ;case of try/catch
-      ((eq? (car (2ndExpression expression)) 'catch) (M_state_TryCatch expression (addLayer initialState (consEmptyListToState state))))
-      ;case of try/finally
-      ((eq? (car (2ndExpression expression)) 'finally) (M_state_TryFinally expression (addLayer initialState (consEmptyListToState state))))
-      (else (error 'unknown "unknown expression")))))
-
-(define M_state_TryCatch
-  (lambda (expression state)
-    (call/cc
-     (lambda (throw)
-       (letrec ((loop (lambda (expression state)
-                        (cond
-                          ((null? expression) state)
-                          (loop (restExpression expression) (M_state (1stExpression expression) state throw (lambda (v) (error "not in loop"))))))))
-         (loop expression state))))))
-
-(define M_state_TryFinally
-  (lambda (expression state)
-    (TryEvaluate (2ndExpression expression)
-                 (letrec ((loop (lambda (expression state)
-                                  (if (null? expression)
-                                      state
-                                      (loop (restExpression expression) (M_state (1stExpression expression) state (lamda (v) v) (lambda (v) v) (lambda (v) (error "not in loop"))))))))
-                   (loop (tryBlock expression) state)))))
-
-(define M_state_TryCatchFinally
-  (lambda (expression state)
-    (TryEvaluate (3rdExpression expression) (TryEvaluate (catchBody expression) (call/cc
-     (lambda (throw)
-       (letrec ((loop (lambda (expression state)
-                        (if (null? expression)
-                            state
-                            (loop (restExpression expression) (M_state (1stExpression expression) state (lambda (v) v) throw (lambda (v) (error "not in loop"))))))))
-         (loop (tryBlock expression) state))))))))
-
-(define TryEvaluate
-  (lambda (expression state)
-    (letrec ((loop (lambda (expression state)
-                     (if (null? expression)
-                         state
-                         (loop (restExpression expression) (M_state (1stExpression expression) state (lambda (v) v) (lambda (v) (error "not in loop")) (lambda (v) (error "not in loop"))))))))
-      (loop expression state))))
+    (
+      (M_state (cons 'begin (cdr expression)) state (lambda (v) v) (lambda (v) (error "not in loop")) (lambda (v) (error "not in loop")))))
               
 (define removeFirstPairFromState
   (lambda (state)
